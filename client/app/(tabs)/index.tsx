1eb06907d5cf57773c5d4f031fd775eb20197f8c
Soumadeep19 <soumadeepputatunda@gmail.com>
2026-01-19 21:50:14 +0530

    Update index.tsx

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -4,8 +4,10 @@ import "../global.css";
 // Home Page
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-3xl font-bold">Welcome</Text>
+    <View  className="flex-1 justify-center items-center">
+      <Text className="text-3xl font-bold text-cardForeground">Welcome</Text>
+      
+  
     </View>
   );
 }
